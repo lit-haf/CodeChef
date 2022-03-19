a01d98f41dea4e568cb6383552fd1e8db673a2b2
ADMIN <ADMIN@DESKTOP-QA8ESVI>
2022-03-19 14:50:20 +0530

    01

---
+++ CodeChef/src/gettingstarted/NumberMirror.java
@@ -0,0 +1,12 @@
+package gettingstarted;
+import java.util.*;
+public class NumberMirror {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		System.out.println(n);
+
+	}
+
+}
