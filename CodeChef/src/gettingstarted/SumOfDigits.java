7d5cd4c31a72a9922cfef5643a59d8a88a8627aa
ADMIN <ADMIN@DESKTOP-QA8ESVI>
2022-03-19 23:51:32 +0530

    04

---
+++ CodeChef/src/gettingstarted/SumOfDigits.java
@@ -0,0 +1,26 @@
+package gettingstarted;
+import java.util.*;
+public class SumOfDigits {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t =sc.nextInt();
+		int sum=0;
+		int temp=0;
+		for(int i=0;i<t;i++)
+		{
+			int n=sc.nextInt();
+			while(n>0)
+			{
+			temp=n%10;
+			sum=sum+temp;
+			n=n/10;	
+			
+			}
+			System.out.println(sum);
+			sum=0;
+	}
+	}
+}
+
+
