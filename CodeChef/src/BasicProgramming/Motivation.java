75b3c318e1cde8ebdc438393f66e6abfb47e3fb4
ADMIN <ADMIN@DESKTOP-QA8ESVI>
2022-03-21 16:19:03 +0530

    03

---
+++ CodeChef/src/BasicProgramming/Motivation.java
@@ -0,0 +1,30 @@
+package BasicProgramming;
+import java.util.*;
+public class Motivation {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("t");
+		int t=sc.nextInt();
+		for(int i=1;i<=t;i++)
+		{
+			int n=sc.nextInt();
+			int x=sc.nextInt();
+			int s[]=new int[n];
+			int r[]=new int[n];
+			int max=0;
+			for(int j=0;j<n;j++)
+			{
+				s[j]=sc.nextInt();
+				r[j]=sc.nextInt();
+				if(s[j]<=x && r[j]>=max)
+				{
+					max=r[j];
+				}
+			}
+			System.out.println(max);
+		}
+		sc.close();
+	}
+
+}
