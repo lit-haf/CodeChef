6dce8b21203063399921b957dc2f784759c1d0b7
ADMIN <ADMIN@DESKTOP-QA8ESVI>
2022-03-21 15:09:02 +0530

    01

---
+++ CodeChef/src/BasicProgramming/RichieRich.java
@@ -0,0 +1,19 @@
+package BasicProgramming;
+import java.util.*;
+public class RichieRich {
+
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int t=sc.nextInt();
+	for(int i=0;i<t;i++)
+	{
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int x=sc.nextInt();
+		int ans=(b-a)/x;
+		System.out.println(ans);
+	}
+
+	}
+
+}
