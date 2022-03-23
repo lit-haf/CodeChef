18c684858148e1b0903b1f12ac376f8d6c38e71a
ADMIN <ADMIN@DESKTOP-QA8ESVI>
2022-03-23 23:24:29 +0530

    08

---
+++ CodeChef/src/basicprogramming/ProgrammingLanguages.java
@@ -0,0 +1,31 @@
+package basicprogramming;
+import java.util.*;
+public class ProgrammingLanguages {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			int a1=sc.nextInt();
+			int b1=sc.nextInt();
+			int a2=sc.nextInt();
+			int b2=sc.nextInt();
+			if((a==a1 && b==b1)||(a==b1 && b==a1))
+			{
+				System.out.println("1");
+			}
+			else if((a==a2 && b==b2)||(a==b2 && b==a2))
+			{
+				System.out.println("2");
+			}
+			else
+			{
+				System.out.println("0");
+			}
+		}
+		}
+
+}
