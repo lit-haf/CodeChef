1581c8a23243ffdf812897dc2635dc7b6065629f
ADMIN <ADMIN@DESKTOP-QA8ESVI>
2022-03-23 15:22:10 +0530

    04

---
+++ CodeChef/src/BasicProgramming/VaccineDates.java
@@ -0,0 +1,26 @@
+package BasicProgramming;
+import java.util.*;
+public class VaccineDates {
+	public static void main(String[] args) {	
+	Scanner sc=new Scanner(System.in);
+	int t=sc.nextInt();
+	for(int i=0;i<t;i++)
+	{
+		int d=sc.nextInt();
+		int l=sc.nextInt();
+		int r=sc.nextInt();
+		if(d>=l && d<=r)
+		{
+			System.out.println("Take second dose now");
+		}
+		else if(d>l)
+		{
+			System.out.println("Too late");
+		}
+		else if(d<r)
+		{
+			System.out.println("Too early");
+		}
+	}
+}
+}
